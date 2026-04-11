e6a3e7e73c97fcc492cb86f0079649157b8b5d12
SachinYadavAug20 <samtagon777@gmail.com>
2026-04-11 14:08:04 +0530

    Fonts and package error solved

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Devflow 
 
 Stackoverflow for students to ask for help with coding.
+
+as of 21-10-2025:
+
+![Screenshot1](Documentation/screenshots/1.png)
--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,12 @@ export default async function Home() {
   return (
     <div>
       <>
-        <h1 className="text-light-500 bg-white align-middle">
-          Hello world
-        </h1>
-        <div className="white-card">
-          Detailed Box of TEXT!!!
-        </div>
-        <code>This is a code block</code>
+        <div className="font-logofont border border-amber-300 rounded-1.5 font-bold text-6xl">DevFlow</div>
+        <h1 className="text-light-500 bg-white align-middle"> Hello world </h1>
+        <div className="card-of-info"> Detailed Box of TEXT!!! </div>
+        <div className="card-of-info font-inter"> Detailed Box of TEXT!!! (inter)</div>
+        <div className="card-of-info font-diff"> Detailed Box of TEXT!!! (diff)</div>
+
       </>
     </div>
   );
