268f39a194c831898b7f849f9154270ce720e4d5
SachinYadavAug20 <samtagon777@gmail.com>
2026-04-19 16:37:11 +0530

    Github auth and toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,50 +3,63 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
 import { cn } from "@/lib/utils";
-import { Inter } from "next/font/google";
-
-const inter = Inter({subsets:['latin'],variable:'--font-sans'});
+import { Toaster } from "sonner";
+import { auth } from "@/auth";
+import { SessionProvider } from "next-auth/react";
 
 const inter = localFont({
-  src:"./fonts/interFont.ttf",
+  src: "./fonts/interFont.ttf",
   variable: "--font-inter",
-  weight:"100 200 300 400 500 600 700 800 900"
+  weight: "100 200 300 400 500 600 700 800 900",
 });
 const diff = localFont({
-  src:"./fonts/diff.ttf",
+  src: "./fonts/diff.ttf",
   variable: "--font-diff",
-  weight:"100 200 300 400 500 600 700 800 900"
+  weight: "100 200 300 400 500 600 700 800 900",
 });
 const logofont = localFont({
-  src:"./fonts/logofont.ttf",
+  src: "./fonts/logofont.ttf",
   variable: "--font-logofont",
-  weight:"100 200 300 400 500 600 700 800 900"
+  weight: "100 200 300 400 500 600 700 800 900",
 });
 
 export const metadata: Metadata = {
   title: "DevFlow",
-  description: "DevFlow provides a platform for jr collge students to directly connect to seniors ofthere college and get answer to there query from the trusted and relaible source.Explore topic like study material, suggested course, best practice, mistakes to avoid, roadmaps to follow and more.",
-  icons:{
-    icon:'/images/site-logo.svg'
-  }
+  icons: {
+    icon: "/images/site-logo.svg",
+  },
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+const RootLayout=async({ children, }: Readonly<{ children: React.ReactNode; }>) =>{
+  const session = await auth();
   return (
     <html
       suppressHydrationWarning
       lang="en"
-      className={cn("h-full", "antialiased", inter.className, diff.variable, logofont.variable, "font-sans", inter.variable)}
+      className={cn(
+        "h-full",
+        "antialiased",
+        inter.className,
+        diff.variable,
+        logofont.variable,
+        "font-sans",
+        inter.variable,
+      )}
     >
-        <body className="min-h-full flex flex-col">
-          <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+      <body className="min-h-full flex flex-col">
+        <SessionProvider session={session}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
             {children}
           </ThemeProvider>
-        </body>
+        </SessionProvider>
+        <Toaster />
+      </body>
     </html>
   );
 }
+export default RootLayout
--- components/forms/socialAuthForm.tsx
+++ components/forms/socialAuthForm.tsx
@@ -1,25 +1,61 @@
-import Image from "next/image"
-import { Button } from "../ui/button"
-import { toast } from "@/hooks/use-toast"
+"use client";
+import Image from "next/image";
+import { Button } from "../ui/button";
+import { toast} from "sonner";
+import { signIn } from "next-auth/react";
+import ROUTES from "@/constant/routes";
 
 const SocialAuthForm = () => {
-  const buttonClass=" background-dark400_light900 body-medium text-dark200_light800 min-h-12 flex-1 rounded-2 px-4 py-3.5";
-  const handleSignIn=async(provider:"github"|"google"|"apple")=>{
+  const buttonClass =
+    " background-dark400_light900 body-medium text-dark200_light800 min-h-12 flex-1 rounded-2 px-4 py-3.5";
+  const handleSignIn = async (provider: "github" | "google" | "apple") => {
     try {
-      
+      // if(provider==='apple'){
+      //   throw new Error("Not implemented");
+      // }
+      await signIn(provider,{
+        callbackUrl: ROUTES.HOME,
+      })
     } catch (error) {
-      console.log(error)
-      
+      console.log(error);
+      toast.error(`Sign-in Failed!`, { position: "top-right" });
     }
-  }
+  };
 
   return (
     <div className="mt-10 flex flex-wrap gap-2.5">
-      <Button className={buttonClass}><Image src="icons/github.svg" alt="Github" width={20} height={20} className="inverted-colors mr-2.5 object-contain"/><span>Log in with Github</span></Button>
-      <Button className={buttonClass}><Image src="icons/google.svg" alt="Github" width={20} height={20} className="mr-2.5 object-contain"/><span>Log in with Google</span></Button>
-      <Button className={buttonClass}><Image src="icons/apple.svg" alt="Github" width={20} height={20} className="inverted-colors mr-2.5 object-contain"/><span>Log in with Apple</span></Button>
+      <Button className={buttonClass} onClick={() => { handleSignIn("github"); }} >
+        <Image
+          src="icons/github.svg"
+          alt="Github"
+          width={20}
+          height={20}
+          className="inverted-colors mr-2.5 object-contain"
+        />
+        <span>Log in with Github</span>
+      </Button>
+      <Button className={buttonClass} onClick={() => { handleSignIn("google"); }} >
+        <Image
+          src="icons/google.svg"
+          alt="Github"
+          width={20}
+          height={20}
+          className="mr-2.5 object-contain"
+        />
+        <span>Log in with Google</span>
+      </Button>
+      <Button className={buttonClass} onClick={() => { handleSignIn("apple");}} >
+        <Image
+          src="icons/apple.svg"
+          alt="Github"
+          width={20}
+          height={20}
+          className="inverted-colors mr-2.5 object-contain"
+        />
+        <span>Log in with Apple</span>
+      </Button>
     </div>
-  )
-}
+  );
+};
 
-export default SocialAuthForm
+export default SocialAuthForm;
