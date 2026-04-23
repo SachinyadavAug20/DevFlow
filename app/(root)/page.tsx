194c004a848d6c560163413b9a518b4038e5c0ae
SachinYadavAug20 <samtagon777@gmail.com>
2026-04-23 14:49:14 +0530

    Made space for left side bar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,28 +7,13 @@ export default async function Home() {
   const session = await auth();
   console.log(session)
   return (
-    <div>
-      <>
-        <h1 className="text-light-500 bg-white align-middle"> Hello world </h1>
-        <div className="card-of-info"> Detailed Box of TEXT!!! </div>
-        <div className="card-of-info font-inter">
-          {" "}
-          Detailed Box of TEXT!!! (inter)
-        </div>
-        {session &&
-        <div className="card-of-info font-diff">
-          {`Hello ${session.user?.name}, your image is:`}
-          <img src={session.user?.image!} alt="User avatar" height={50} width={50} />
-          <p>{session.user?.email}</p>
-        </div>}
-        <Button className="text-2xl mx-[45vw] my-10">Hello shadCN</Button>
-        <form className="px-10 pt-25" action={async()=>{
+    <div className="py-21">
+        <form className="pt-1" action={async()=>{
           "use server"
           await signOut({redirectTo:ROUTES.SIGN_IN})
         }}>
           <Button type="submit">Log out</Button>
         </form>
-      </>
     </div>
   );
 }
---
+++ components/navigation/Leftsidebar.tsx
@@ -0,0 +1,12 @@
+import { Section } from "lucide-react"
+
+const Leftsidebar = () => {
+  return (
+    <Section>
+      <div
+      </div>
+    </Section>
+  )
+}
+
+export default Leftsidebar
