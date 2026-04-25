b72f8848e58ba3b4b2a02d6461d02c8e90db921e
SachinYadavAug20 <samtagon777@gmail.com>
2026-04-25 12:45:40 +0530

    Basic structure of home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,19 +1,31 @@
 import { Button } from "@/components/ui/button";
 import { auth } from "@/auth";
-import { signOut } from "@/auth"
+import { signOut } from "@/auth";
 import ROUTES from "@/constant/routes";
+import Link from "next/link";
 
 export default async function Home() {
   const session = await auth();
-  console.log(session)
+  console.log(session);
   return (
-    <div className="py-21">
-        <form className="pt-1" action={async()=>{
-          "use server"
-          await signOut({redirectTo:ROUTES.SIGN_IN})
-        }}>
-          <Button type="submit">Log out</Button>
-        </form>
-    </div>
+    <>
+      <section className="w-full flex flex-col-reverse sm:flex-row justify-between gap-4 sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+        <Button
+          className="primary-gradient min-h-11.5 px-4 py-3 text-light-900!"
+          asChild
+        >
+          <Link href={ROUTES.ASKQUESTION}>Ask a Question</Link>
+        </Button>
+      </section>
+      <section className="mt-11">LocalSearch</section>
+      HomeFilter
+      <div className="mt-10 flex w-full flex-col gap-6">
+        <p>Question card 1</p>
+        <p>Question card 2</p>
+        <p>Question card 3</p>
+        <p>Question card 4</p>
+      </div>
+    </>
   );
 }
--- components/navigation/Leftsidebar.tsx
+++ components/navigation/Leftsidebar.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { Button } from "../ui/button";
 import ROUTES from "@/constant/routes";
 import { useSession } from "next-auth/react";
+import { signOut } from "next-auth/react";
 import NavLinks from "./navbar/NavLinks";
 
 const Leftsidebar = () => {
@@ -16,7 +17,10 @@ const Leftsidebar = () => {
       <div className="flex flex-col gap-3">
         {session.status === "unauthenticated" && (
           <div className="flex flex-col gap-3 ">
-            <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none background-light900_dark200! text-dark500_light700!" asChild>
+            <Button
+              className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none background-light900_dark200! text-dark500_light700!"
+              asChild
+            >
               <Link href={ROUTES.SIGN_IN}>
                 <Image
                   src="/icons/account.svg"
@@ -31,7 +35,10 @@ const Leftsidebar = () => {
               </Link>
             </Button>
 
-            <Button className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg border px-4 py-3 shadow-none background-light700_dark400! text-dark500_light400!" asChild>
+            <Button
+              className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg border px-4 py-3 shadow-none background-light700_dark400! text-dark500_light400!"
+              asChild
+            >
               <Link href={ROUTES.SIGN_UP}>
                 <Image
                   src="/icons/sign-up.svg"
@@ -40,9 +47,40 @@ const Leftsidebar = () => {
                   height={20}
                   className="invert-colors lg:hidden"
                 />
-              <span className="max-lg:hidden">Sign Up</span>
+                <span className="max-lg:hidden">Sign Up</span>
               </Link>
             </Button>
+          </div>
+        )}
+        {session.status === "authenticated" && (
+          <div className="flex flex-col gap-2 justify-center items-center">
+            <Button
+              className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none background-light900_dark200! text-dark500_light700!"
+              asChild
+            >
+              <Link href={ROUTES.SIGN_IN}>
+                <img
+                  src={`${session.data.user?.image}`}
+                  alt="account"
+                  width={25}
+                  height={25}
+                  className="invert-colors rounded-4xl"
+                />
+                <span className="primary-text-gradient max-lg:hidden max-lg:hidden">
+                  {session.data.user?.name}
+                </span>
+              </Link>
+            </Button>
+            <div>
+              <form
+                className="pt-1"
+                action={async () => {
+                  await signOut({ redirectTo: ROUTES.SIGN_IN });
+                }}
+              >
+                <Button type="submit">Log out</Button>
+              </form>
+            </div>
           </div>
         )}
       </div>
