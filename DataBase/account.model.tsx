82f4b525ba826531fc5564f96496b318fe512f0b
SachinYadavAug20 <samtagon777@gmail.com>
2026-04-28 14:29:13 +0530

    Account model schema

---
+++ DataBase/account.model.tsx
@@ -0,0 +1,23 @@
+import { model,Schema,models, Types } from "mongoose";
+
+
+export interface IAccount {
+  userId: Types.ObjectId;
+  name:string;
+  image?:string;
+  password?:string;
+  provider:string;
+  providerAccountId:string;
+}
+
+const AccountSchema = new Schema<IAccount>({
+  userId: { type: Schema.Types.ObjectId, ref:'User',required:true },
+  name:{type:String,require:true},
+  image:{type:String},
+  password:{type:String},
+  provider:{type:String,require:true},
+  providerAccountId:{type:String,require:true},
+},{timestamps:true});
+
+const Account =models?.Account || model<IAccount>("Account", AccountSchema);
+export default Account
